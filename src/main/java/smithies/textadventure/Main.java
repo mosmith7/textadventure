a69f8f566a9e4c9b3ba5047d75c732034983becc
Mark Smithies <mark.smithies@mpec.co.uk>
2018-10-18 21:02:39 +0100

    Spring bootified app in case it is needed

--- src/main/java/smithies/textadventure/Main.java
+++ src/main/java/smithies/textadventure/Main.java
@@ -1,7 +1,9 @@
 package smithies.textadventure;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import smithies.textadventure.session.SessionManager;
 
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
